c15fc00e5b21f0034ea54e77ecb86ae0312b8e60
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-27 17:59:24 -0300

    added endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,30 @@
-console.log("hello world");
+import fastify from "fastify";
+import { request } from "http";
+
+const server = fastify({ logger: true });
+
+const teams = [
+  { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
+  { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
+  { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+];
+
+const drivers = [
+  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
+  { id: 2, name: "Lando Norris", team: "Ferrari" },
+];
+
+server.get("/teams", async (request, response) => {
+  response.type("application/json").code(200);
+  return { teams };
+});
+
+server.get("/drivers", async (request, response) => {
+  response.type("application/json").code(200);
+  return { drivers };
+});
+
+server.listen({ port: 3333 }, () => {
+  console.log("Server init");
+});
