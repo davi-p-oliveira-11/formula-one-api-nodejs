c93c6e929f266de46fabe1dc2d245d64eb2ac285
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-27 18:39:04 -0300

    added more data

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,13 @@ const teams = [
   { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
   { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
   { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+  { id: 4, name: "Ferrari", base: "Maranello, Italy" },
+  { id: 5, name: "Aston Martin", base: "Silverstone, United Kingdom" },
+  { id: 6, name: "Alpine", base: "Enstone, United Kingdom" },
+  { id: 7, name: "AlphaTauri", base: "Faenza, Italy" },
+  { id: 8, name: "Williams", base: "Grove, United Kingdom" },
+  { id: 9, name: "Haas", base: "Kannapolis, United States" },
+  { id: 10, name: "Alfa Romeo", base: "Hinwil, Switzerland" },
 ];
 
 const drivers = [
